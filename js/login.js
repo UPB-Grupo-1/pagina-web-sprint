6bcdea560cfea44f720be696531446346429cf95
boudgnosis <dragon30_pablo@outlook.com>
2021-10-15 17:39:38 -0500

    v1.4

---
+++ js/login.js
@@ -0,0 +1,18 @@
+let registros = []
+
+function login() {
+	
+}
+
+function agregarRegistro() {
+	
+}
+
+function validarCAPTCHA(valor) {
+	
+}
+
+module.exports.login  = login;
+module.exports.registros  = registros;
+module.exports.validarCAPTCHA  = validarCAPTCHA;
+module.exports.agregarRegistro  = agregarRegistro;
