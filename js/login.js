7de236f600b66979cfa6b372657d50a04421d9b6
boudgnosis <dragon30_pablo@outlook.com>
2021-10-15 18:04:44 -0500

    v1.4.1

--- js/login.js
+++ js/login.js
@@ -8,9 +8,18 @@ function agregarRegistro() {
 	
 }
 
+let captcha = document.getElementById('captcha').value
+let numberCaptcha = parseInt(captcha)
+
 function validarCAPTCHA(valor) {
-	
+	if (valor === 15) {
+		return true
+	} else {
+		return false
+	}
 }
+
+validarCAPTCHA(numberCaptcha)
 
 module.exports.login  = login;
 module.exports.registros  = registros;
