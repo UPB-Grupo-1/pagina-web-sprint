673fdd0039d4172d436625dbb2b8f33fe4b6a271
boudgnosis <dragon30_pablo@outlook.com>
2021-10-16 20:47:20 -0500

    v1.5

--- js/login.js
+++ js/login.js
@@ -1,32 +1,46 @@
 let registros = []
 
-let email = document.getElementById('correo').value
-function login() {
-	if (email == registros.length()) {
-		return true
-	}else {
-		return false
-	}
+function login(){
+    let email = document.getElementById('correo').value
+    let contrasena = document.getElementById('contrasena').value
+    let buscarEmail = registros.find(o => o.correo === email)
+    let buscarContrasena = registros.find(o => o.contrasena === contrasena)
+    if(buscarEmail && buscarContrasena) {
+        validarCAPTCHA()
+        return true
+    }
+    else{
+         return false
+    }
 }
 
 function agregarRegistro() {
-	
+  
+    function crearObjetoRegistro(n, a, t, c, co) {
+      return { nombre: n, apellido: a, telefono: t, correo: c, contrasena: co }
+    }
+  
+    var nombre = document.getElementById('nombre').value
+    var apellido = document.getElementById('apellido').value
+    var telefono = document.getElementById('telefono').value
+    var correo = document.getElementById('correo').value
+    var contrasena = document.getElementById('contrasena').value
+  
+    var miRegistro = new crearObjetoRegistro(nombre,apellido,telefono,correo,contrasena)
+  
+    registros.push(miRegistro)
 }
 
-let captcha = document.getElementById('captcha').value
-let numberCaptcha = parseInt(captcha)
-
-function validarCAPTCHA(valor) {
-	if (valor === 15) {
-		return true
-	} else {
-		return false
-	}
+function validarCAPTCHA(valor){
+    if (valor == 15){
+        return true
+    }
+    else{
+        return false
+    }
 }
 
-validarCAPTCHA(numberCaptcha)
-
-module.exports.login  = login;
-module.exports.registros  = registros;
-module.exports.validarCAPTCHA  = validarCAPTCHA;
-module.exports.agregarRegistro  = agregarRegistro;
+module.exports.login = login
+module.exports.registros = registros
+module.exports.validarCAPTCHA = validarCAPTCHA
+module.exports.agregarRegistro = agregarRegistro
